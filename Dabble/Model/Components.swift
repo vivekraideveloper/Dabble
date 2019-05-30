1aa3288a57d0a70acd9eefce12fcc91d4ec81acb
Vivek Rai <vivekraiiosdev@gmail.com>
2019-05-30 11:02:29 +0530

    Completed Keyboard toggle

--- Dabble/Model/Components.swift
+++ Dabble/Model/Components.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Components {
+    
+    private(set) public var title: String
+    private(set) public var imageName: String
+    private(set) public var color1: Int
+    private(set) public var color2: Int
+
+    init(title: String, imageName: String, color1: Int, color2: Int) {
+        self.title = title
+        self.imageName = imageName
+        self.color1 = color1
+        self.color2 = color2
+    }
+}
+
